62261deaa53d1d47e4884b13d49a4f5e2132798a
deepak mohan <krishna66827@outlook.com>
2017-09-18 15:52:15 +0530

    interchanging debit and credit strings

---
+++ LoginSignUpService/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoginSignUpService
+{
+    public class Constants
+    {
+        public static readonly int OTPLength = 6;
+        public static readonly string CreditTransactionSMSFormat = "OTP for Deducting INR {0} from your wallet at {1} is {2}. Please share it with merchant only for security reasons.";
+        public static readonly string DebitTransactionSMSFormat = "OTP for Adding INR {0} to your wallet at {1} is {2}. Please share it with merchant only for security reasons.";
+    }
+}
