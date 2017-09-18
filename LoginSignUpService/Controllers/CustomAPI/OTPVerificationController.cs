350a767bb3f493956ef687854230a9e608bcf262
deepak mohan <krishna66827@outlook.com>
2017-09-18 13:16:18 +0530

    SMS OTP Verification

---
+++ LoginSignUpService/Controllers/CustomAPI/OTPVerificationController.cs
@@ -0,0 +1,75 @@
+﻿using LoginSignUpService.DTO;
+using LoginSignUpService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LoginSignUpService.Controllers.CustomAPI
+{
+    public class OTPVerificationController : ApiController
+    {
+        private LoginSignUpServiceContext db = new LoginSignUpServiceContext();
+
+        [HttpGet]
+        [ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> Get(OTPVerificationDTO OTPVerificationDTO)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (OTPVerificationDTO == null)
+                throw new Exception("OTP Verification should not have been null");
+
+            if (OTPVerificationDTO.UserID == Guid.Empty)
+                return Ok("123456");
+
+            var user = await db.Users.FindAsync(OTPVerificationDTO.UserID);
+            if (user == null)
+                throw new Exception(String.Format("UserId {0} does not exist.", OTPVerificationDTO.UserID));
+
+            if (user.NumberOfSMS < 1)
+                return BadRequest("Please recharge your SMS Balance.\nCall @7987421819");
+
+            var OTP = GenerateOTP();
+            var SMSContent = GenerateSMSContent(OTPVerificationDTO, user, OTP);
+
+            //TODO: CALL SMS API(mobile number, SMSContent)
+
+            DeductSMSBalance(user);
+            await db.SaveChangesAsync();
+
+            return Ok(OTP);
+        }
+
+        private void DeductSMSBalance(User user)
+        {
+            user.NumberOfSMS--;
+            db.Entry(user).State = EntityState.Modified;
+        }
+
+        private string GenerateSMSContent(OTPVerificationDTO dto, User user, string OTP)
+        {
+            string SMSContent = "";
+            if (dto.IsCredit == true)
+                SMSContent = String.Format(Constants.CreditTransactionSMSFormat, dto.TransactionAmount, user.BusinessName, OTP);
+            else if (dto.IsCredit == false)
+                SMSContent = String.Format(Constants.DebitTransactionSMSFormat, dto.TransactionAmount, user.BusinessName, OTP);
+            return SMSContent;
+        }
+
+        private string GenerateOTP()
+        {
+            var random = new Random();
+            string OTP = "";
+            for (int i = 0; i < Constants.OTPLength; i++)
+                OTP = String.Concat(OTP, random.Next(10).ToString());
+            return OTP;
+        }
+    }
+}
---
+++ LoginSignUpService/DTO/OTPVerificationDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoginSignUpService.DTO
+{
+    public class OTPVerificationDTO
+    {
+        [Required]
+        public Guid? UserID { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        public string ReceiverMobileNo { get; set; }
+
+        [Required]
+        [Range(0, 98765432198765)]
+        public decimal? TransactionAmount { get; set; }
+
+        [Required]
+        public bool? IsCredit { get; set; }
+
+    }
+}
